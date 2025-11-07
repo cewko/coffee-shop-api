47b0d07c3d69b4ab7d28207eaf786771ef7c2ad0
cewko <221901489+cewko@users.noreply.github.com>
2025-11-07 16:35:39 +0100

    feat: configure i18n with polish and english language support

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONBUFFERED=1
+RUN apt-get update && apt-get install -y gettext
 WORKDIR /src
 COPY requirements.txt /src/
 RUN pip install -r requirements.txt
--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,6 +120,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGES = (
+    ("pl", "polish"),
+    ("en", "english")
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "locale"),
+)
 
 TIME_ZONE = 'UTC'
 
