1efebc9f3e45747bebd39a57beaeb6df3262f046
cewko <221901489+cewko@users.noreply.github.com>
2025-11-07 16:47:14 +0100

    feat: create custom user model extending abstract base user

--- authx/models.py
+++ authx/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser
 
-# Create your models here.
+from .common import ROLES
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.PositiveIntegerField(unique=False, null=True, blank=True)
+
+    is_active = models.BooleanField(_("Is active User"), default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    
+    role = models.PositiveIntegerField(
+        _("User role"), choices=ROLES, default=1
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    @property
+    def full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    def __str__(self):
+        return str(self.full_name)
