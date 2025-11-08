0d85810865a3d2be32eb3473e940b3dfcdb45822
cewko <221901489+cewko@users.noreply.github.com>
2025-11-08 11:32:29 +0100

    feat: implement custom jwt creation utility

---
+++ authx/jwt.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from datetime import datetime
+
+import base64
+import json
+import hmac
+import hashlib
+
+
+def default_converter(o):
+    if isinstance(0, datetime):
+        return o.__str__()
+
+
+def decode_jwt(data):
+    return base64.urlsafe_b64decode(data)
+
+
+def base64url_encode(data):
+    bytesString = data.encode("ascii")
+    return base64.urlsafe_b64encode(bytesString).decode("utf-8")
+
+
+def create_jwt(payload):
+    header = {
+        "alg": "HS256",
+        "typ": "JWT"
+    }
+
+    secret_key = settings.SECRET_KEY
+    total_params = str(base64url_encode(json.dumps(header))) + "." \
+        + str(base64url_encode(json.dumps(
+            payload, default=default_converter
+        )))
+
+    signature = hmac.new(
+        secret_key.encode(), 
+        total_params.encode(), 
+        hashlib.sha256
+    ).hexdigest()
+
+    token = total_params + "." + str(base64url_encode(signature))
+
+    return token
