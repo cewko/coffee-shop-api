41df44245eb00fdac28af7c96f952c3f5a645b52
cewko <221901489+cewko@users.noreply.github.com>
2025-11-08 11:12:01 +0100

    feat: override destroy method to logout user on account deletion

--- authx/viewsets.py
+++ authx/viewsets.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, logout
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from .permissions import IsOwner
 from .serializers import UserSerializer
 
 
@@ -8,6 +11,12 @@ User = get_user_model()
 
 
 class UserViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwner]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        logout(request)
+        self.perform_destroy(instance)
+        return Response(status=HTTP_204_NO_CONTENT)
