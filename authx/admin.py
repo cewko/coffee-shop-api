998fe7f03b9c9e80e3de22743ea6909508e8e76c
cewko <221901489+cewko@users.noreply.github.com>
2025-11-10 18:54:58 +0100

    feat: register blacklisted tokens in the admin interface & add custom user admin

--- authx/admin.py
+++ authx/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from authx.models import CustomUser
+from authx.models import CustomUser, BlackListToken
 
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "role", "is_active"]
+    list_filter = ["role", "is_active"]
+    search_fields = ["username", "email"]
+
+
+@admin.register(BlackListToken)
+class BlackListTokenAdmin(admin.ModelAdmin):
+    list_display = ["user", "blacklisted_at", "reason"]
+    list_filter = ["blacklisted_at", "reason"]
+    search_fields = ["user__username", "user__email", "token"]
+    readonly_fields = ["token", "user", "blacklisted_at"]
