50f9d0ee1cf72cbca990a60492828c335fa9b068
cewko <221901489+cewko@users.noreply.github.com>
2025-11-11 18:26:36 +0100

    feat: add supplier model with admin registaration

--- supplier/models.py
+++ supplier/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator, MinLengthValidator
 
-# Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(
+        _("Supplier Name"),
+        max_length=50,
+        validators=[MinLengthValidator(4)],
+        null=False,
+        blank=False
+    )
+    phone_regex = RegexValidator(
+        regex=r"^\+?[\d]{4,15}$",
+        message="Phone number must be entered in the format: '+49301234567', \
+            Up to 15 digits is allowed."
+    )
+    phone_number = models.CharField(
+        _("Phone number"),
+        validators=[phone_regex],
+        max_length=17,
+        blank=False,
+        null=False
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-name",)
+    
+    @property
+    def custom_id(self):
+        phone_part = self.phone_number.replace("+", "")[-2:]
+        name_part = self.name[:2].upper()
+        year = self.created_date.year
+
+        return f"{phone_part}-{name_part}-{year}"
+
+    def __str__(self):
+        return self.custom_id
