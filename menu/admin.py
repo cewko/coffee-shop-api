5cafc2a8bd7819a50ca378504bdb555a07d1adb5
cewko <221901489+cewko@users.noreply.github.com>
2025-11-12 16:00:53 +0100

    feat: add menu models for product management

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Component, MenuItem, Menu
 
-# Register your models here.
+admin.site.register(Menu)
+admin.site.register(MenuItem)
+admin.site.register(Component)
