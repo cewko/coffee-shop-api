29bedb8e71b227645a3c6b182e45a8d0b7e734e7
cewko <221901489+cewko@users.noreply.github.com>
2025-11-16 16:52:22 +0100

    refactor(permissions): use inheritance and apply some dry

--- authx/permissions.py
+++ authx/permissions.py
@@ -1,35 +1,55 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsCashier(BasePermission):
+class RolePermission(BasePermission):
+    required_role = 1
+
     def has_permission(self, request, view):
-        return bool(request.user.is_authenticated and request.user.role >= 1)
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.user.role >= required_role
+        )
 
 
-class IsBarista(BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user.is_authenticated and request.user.role >= 2)
+class IsCashier(RolePermission):
+    required_role = 1
 
 
-class IsManager(BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user.is_authenticated and request.user.role >= 3)
-    
+class IsBarista(RolePermission):
+    required_role = 2
+
+
+class IsManager(RolePermission):
+    required_role = 3
+
 
 class IsOwner(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.role == 4 or request.user.is_superuser)
+        return (
+            request.user
+            and request.user.is_authenticated
+            and (request.user.role == 4 or request.user.is_superuser)
+        )
 
 
 class MenuViewPermission(BasePermission):
     def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+
         if view.action in ["retrieve", "list"]:
-            return bool(request.user.is_authenticated and request.user.role >= 1)
+            return request.user.role >= 1
+            
         elif view.action in ["update", "partial_update", "destroy", "create"]:
-            return bool(request.user.is_authenticated and request.user.role >= 2)
+            return request.user.role >= 2
 
     def has_object_permission(self, request, view, obj):
+        if not request.user or not request.user.is_authenticated:
+            return False
+
         if view.action == "retrieve":
-            return bool(request.user.is_authenticated and request.user.role >= 1)
-        elif view.action in ["update", "partial_update", "destroy", "create"]:
-            return bool(request.user.is_authenticated and request.user.role >= 2)
+            return request.user.role >= 1
+
+        elif view.action in ["update", "partial_update", "destroy"]:
+            return request.user.role >= 2
