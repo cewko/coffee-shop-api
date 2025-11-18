cdd2ebcf9a8c0943f0b23ea91afe067f0b3c00f4
cewko <221901489+cewko@users.noreply.github.com>
2025-11-18 11:07:23 +0100

    feat: add purchase order model and register it in the admin interface

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class PurchaseOrder(models.Model):
+    STATUS_CHOICE = (
+        (1, _("New")),
+        (2, _("Pending")),
+        (3, _("Ready")),
+        (4, _("Retrieved"))
+    )
+    items = models.ManyToManyField("menu.MenuItem")
+    status = models.PositiveIntegerField(
+        choices=STATUS_CHOICE,
+        default=1
+    )
+    order_number = models.CharField(max_length=50)
+    client_name = models.CharField(max_length=50, blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.order_number} - {self.get_status_display()}"
