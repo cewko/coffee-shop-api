709e1379b48cd5f50fbf4537cfad08057aafa7d9
cewko <221901489+cewko@users.noreply.github.com>
2025-11-18 12:01:52 +0100

    feat: configure url routing for purchase endpoints

--- purchase/urls.py
+++ purchase/urls.py
@@ -2,9 +2,17 @@ from django.urls.conf import include
 from django.urls import re_path
 
 from rest_framework.routers import DefaultRouter
+from .views import PurchaseListView
+from .viewsets import PurchaseViewSet
 
 router = DefaultRouter()
+router.register(
+    r"purchase",
+    PurchaseViewSet,
+    basename="purchase"
+)
 
 urlpatterns = [
-    re_path(r"", include(router.urls))
+    re_path(r"", include(router.urls)),
+    path("purchase-list", PurchaseListView.as_view(), name="purchase-list")
 ]
